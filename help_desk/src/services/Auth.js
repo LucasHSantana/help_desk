336a89561648ad6cf88a46fe62eed691689469a6
Lucas Henrique Santana <lukaz.santana@gmail.com>
2019-09-04 23:59:56 -0300

    Criado login lado cliente

--- help_desk/src/services/Auth.js
+++ help_desk/src/services/Auth.js
@@ -1,9 +1,12 @@
-export const TOKEN_KEY = "login_token";
+const TOKEN_KEY = 'login_token';
+const IV_KEY = 'iv_token'
+
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 
-export const login = token => {
+export const login = (token, iv) => {
   localStorage.setItem(TOKEN_KEY, token);
+  localStorage.setItem(IV_KEY, iv);
 };
 
 export const logout = () => {
